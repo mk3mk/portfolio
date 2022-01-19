da63e0c74d75d14e0bf62cb017e2d33fe8459c30
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-19 15:05:38 +0300

    Home is done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import {CallToAction} from "./portfolio/components/callToAction/CallToAction";
 import {Contacts} from "./portfolio/components/contacts/Contacts";
 import {Footer} from "./portfolio/components/footer/Footer";
 import {AboutMe} from "./portfolio/components/aboutMe/AboutMe";
+import {Home} from "./portfolio/components/home/Home";
+import homeImg from './portfolio/assets/home/bg-home-image.jpg'
+import aboutImg from './portfolio/assets/aboutMe/about-image.jpg'
 
 function App() {
   return (
     <div className="App">
-      <AboutMe urlImg={"https://trydo.rainbowit.net/assets/images/about/about-8.jpg"}/>
+      <Home urlImage={homeImg}/>
+      <AboutMe urlImg={aboutImg}/>
       <MySkills/>
       <MyLatestProjects/>
       <CallToAction/>
---
+++ src/portfolio/components/home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './home.scss'
+
+export const Home = ({urlImage}) => {
+  return (
+    <div className="home " style={{ backgroundImage: `url(${urlImage})` }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="inner">
+              <span className="pretitle">Welcome to my World</span>
+              <h1 className="title_row1">Hi, I’m Alex Nikitin</h1>
+              <h1 className="title_row2">WEB Developer.</h1>
+              <h2 className="subtitle">based in BELARUS.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
